54bd3d682f7c0d7e5489521c0e27b588f1b74c58
Kartik-Malviya68 <kartikpc68@gmail.com>
2025-10-13 20:09:56 +0530

    refactor: consolidate app setup into index.ts and remove unused files

--- src/dev-server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-// pick any port (not 3000 if you also run vercel dev)
-const PORT = Number(process.env.PORT ?? 4000);
-
-app.listen(PORT, () => {
-  console.log(`Dev server running on http://localhost:${PORT}`);
-});
--- src/handler.ts
+++
@@ -1,3 +0,0 @@
-import serverless from 'serverless-http'
-import app from './app'
-export default serverless(app)                 // ESM default export
